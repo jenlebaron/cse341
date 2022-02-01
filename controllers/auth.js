54a6a60a2bb53516c450e2adbee5ea17156cc80f
Jennifer LeBaron <JenLeBaron@msn.com>
2022-02-01 12:25:04 -0800

    is working again after section 14

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('61f991d948d9b71faa6396ea')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
