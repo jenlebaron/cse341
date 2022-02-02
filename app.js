20e90275c30826c13ae8b3ca8ebddd8b8501aadf
Jennifer LeBaron <JenLeBaron@msn.com>
2022-02-01 16:46:16 -0800

    finished section 15

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -19,6 +21,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -37,7 +40,8 @@ app.use(
     store: store
   })
 );
-
+app.use(csrfProtection);
+app.use(flash());
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -48,6 +52,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -75,21 +85,8 @@ const MONGODB_URL = process.env.MONGODB_URL || MONGODB_URI;
 
 mongoose
   .connect(
-    MONGODB_URL, options
-  )
+    MONGODB_URL, options)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Jen', 
-          email: 'jen@test.com', 
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    })
     app.listen(PORT);
   })
   .catch(err => {
