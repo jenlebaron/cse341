6c6ff1ee821ef4b1fb7f4a48d3bec41daf526c54
Jen LeBaron <77174363+jenlebaron@users.noreply.github.com>
2022-02-07 20:34:18 -0800

    finished understanding validation

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 const express = require('express');
+const {
+    check, body
+} = require('express-validator/check');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
@@ -8,7 +11,20 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get('/add-product', [
+    body('title')
+        .isString()
+        .isLength ({ min: 3 })
+        .trim(),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isLength ({ min: 5, max: 400 })
+        .trim()
+],
+ isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
@@ -18,7 +34,22 @@ router.post('/add-product', isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+    '/edit-product',
+    [
+        body('title')
+            .isString()
+            .isLength ({ min: 3 })
+            .trim(),
+        body('imageUrl')
+            .isURL(),
+        body('price')
+            .isFloat(),
+        body('description')
+            .isLength ({ min: 5, max: 400 })
+            .trim() 
+    ],
+    isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
